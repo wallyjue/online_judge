649f952a9ccff02834c26589597a40aa11286e7f
wally.chang <wally.chang@protolabs.co.jp>
2018-03-14 17:17:55 +0900

    Adding problem 1

---
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem_1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace online_judge.leetcode.easy
+{
+    class Problem_1
+    {
+		public int[] TwoSum(int[] nums, int target)
+		{
+			int ret1 = 0, ret2 = 0;
+			for(int cnt = 0; cnt < nums.Length; cnt++)
+			{
+				for(int cnt2 = cnt+1; cnt2 < nums.Length; cnt2++)
+				{
+					if(nums[cnt] + nums[cnt2] == target)
+					{
+						ret1 = cnt;
+						ret2 = cnt2;
+						break;
+					}
+				}
+			}
+
+
+			int[] ret = { ret1, ret2 };
+			return ret;
+		}
+	}
+}
