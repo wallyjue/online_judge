c6fe44e9ff40741637c69b42d86b50a0a12c8350
wally.chang <wally.chang@protolabs.co.jp>
2018-03-23 17:05:01 +0900

    Adding problem 7,8

---
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem_7.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.easy
+{
+    class Problem_7
+    {
+		public int Reverse(int x)
+		{
+			for(int radix = 0; radix < 10; radix++)
+			{
+
+			}
+
+			return 0;
+		}
+	}
+}
---
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem_8.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace online_judge.leetcode.medium
+{
+    class Problem_8
+    {
+		public int MyAtoi(string str)
+		{
+			string input = str.Trim();
+			bool isNegative = false;
+			char[] data = input.ToCharArray();
+			isNegative = data[0] == '-' ? true : false;
+
+			for(int cnt = 0; cnt < data.Length;cnt++)
+			{
+				int a = '0';
+			}
+
+			return 0;
+		}
+	}
+}
