cfb1165925faa7fd7ea658cd0e64bf7c0edb8862
wally.chang <wally.chang@protolabs.co.jp>
2018-05-29 17:39:07 +0900

    add 69

---
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem_69.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace online_judge.leetcode.easy
+{
+    class Problem_69
+    {
+		public int MySqrt(int x)
+		{
+			int ret = 1;
+			while( (x/ret) >= ret)
+			{
+				ret++;
+			}
+			return ret-1;
+		}
+	}
+}
