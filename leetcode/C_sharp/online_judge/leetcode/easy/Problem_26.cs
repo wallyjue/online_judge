5b9131c30d1c842c2274f9b1ee21f6a838582893
wally.chang <wally.chang@protolabs.co.jp>
2018-05-29 15:57:18 +0900

    solution to 26

---
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem_26.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace online_judge.leetcode.easy
+{
+    class Problem_26
+    {
+		public int RemoveDuplicates(int[] nums)
+		{
+			Dictionary<int, int> keyCount = new Dictionary<int, int>();
+			for(int cnt = 0; cnt < nums.Length; cnt++)
+			{
+				int valueCount = keyCount.GetValueOrDefault(nums[cnt]);
+				keyCount.Remove(nums[cnt]);
+				keyCount.Add(nums[cnt], valueCount+1);
+			}
+			int next = Int32.MinValue, pivot = 0;
+			for (int cnt = 0; cnt < nums.Length; cnt++)
+			{
+				if( next < nums[cnt]) 
+				{
+					nums[pivot] = nums[cnt];
+					next = nums[pivot];
+					pivot++;
+				}
+			}
+			return keyCount.Count;
+		}
+
+	}
+}
