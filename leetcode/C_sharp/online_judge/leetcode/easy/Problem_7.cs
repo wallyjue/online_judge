ed366dc423d6755f8821d4adb1c9903816223e55
wally.chang <wally.chang@protolabs.co.jp>
2018-05-29 16:51:03 +0900

    Add 7

--- leetcode/C_sharp/online_judge/leetcode/easy/Problem_7.cs
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem_7.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,26 @@ namespace online_judge.leetcode.easy
     {
 		public int Reverse(int x)
 		{
-			for(int radix = 0; radix < 10; radix++)
-			{
+			bool isNegative = x < 0;
+			int[] numbers = new int[10];
+			int decimals = 0;
+			int ret = 0;
+			x = isNegative ? -x : x;
 
+			for(int radix = 0, number = 0, mod = 10, power = 1; radix < 10; radix++)
+			{
+				numbers[radix] = radix < 9 ? (x % mod) / power : x / power;
+				if (numbers[radix] != 0) decimals = radix;
+				mod *= 10;
+				power *= 10;
 			}
 
-			return 0;
+			for (int cnt = 0; cnt <= decimals; cnt++)
+			{
+				if (ret * 10 / 10 != ret) return 0;
+				ret = ret * 10 + numbers[cnt];
+			}
+			return isNegative ? -ret : ret;
 		}
 	}
 }
