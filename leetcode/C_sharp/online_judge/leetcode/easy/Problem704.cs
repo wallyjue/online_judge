9795e8f5d224b95286aea98394eda8472be6853a
wallyjue <wallyjue@gmail.com>
2022-03-04 12:57:09 +0800

    Adding vsc configuration

---
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem704.cs
@@ -0,0 +1,69 @@
+﻿using System.Text;
+
+namespace online_judge.leetcode.easy
+{
+    public class Problem704
+    {
+        public int Search(int[] nums, int target)
+        {
+            if (nums.Length <= 2)
+            {
+                if (nums[0] == target)
+                {
+                    return 0;
+                }
+                else if (nums.Length == 2 && nums[1] == target)
+                {
+                    return 1;
+                }
+
+                return -1;
+            }
+
+
+            int index = nums.Length / 2;
+            for (var cnt = 0; cnt < nums.Length; cnt++)
+            {
+                if( target == nums[index])
+                {
+                    return index;
+                }
+
+                int diff = index / 2;
+                
+                if (diff < 1)
+                {
+                    diff = 1;
+                }
+
+                if (target > nums[index])
+                {
+                    index += diff;
+                    if (target == nums[index])
+                    {
+                        return index;
+                    }
+                    else if (index > nums.Length)
+                    {
+                        return -1;
+                    }
+                }
+                else
+                {
+                    index -= diff;
+                    if (target == nums[index])
+                    {
+                        return index;
+                    }
+                    else if (index < 0)
+                    {
+                        return -1;
+                    }
+                }
+
+            }
+            return -1;
+
+        }
+    }
+}
---
+++ leetcode/C_sharp/online_judge/leetcode/explore/Array.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.explore
+{
+    class Array
+    {
+        public int FindNumbers(int[] nums)
+        {
+            int ret = 0;
+            for (int cnt = 0; cnt < nums.Length; cnt++)
+            {
+                int digit = GetDigit(nums[cnt]);
+                if (digit % 2 == 0)
+                {
+                    ret++;
+                }
+            }
+
+            return ret;
+        }
+
+
+        public int GetDigit(int input)
+        {
+            int digit = 1;
+
+            int mod = input / 10;
+            while (mod > 0)
+            {
+                digit++;
+                mod = mod / 10;
+
+            }
+            return digit;
+        }
+
+        public int[] SortedSquares(int[] nums)
+        {
+            int[] ret = new int[nums.Length];
+            int lcnt = 0, rcnt = nums.Length-1;
+            int cnt = nums.Length-1;
+            for(cnt = nums.Length-1; cnt >= 0; cnt--)
+            {
+                int lcntAbs = nums[lcnt] >= 0 ? nums[lcnt] : -nums[lcnt];
+                int rcntAbs = nums[rcnt] >= 0 ? nums[rcnt] : -nums[rcnt];
+                if (lcntAbs < rcntAbs) 
+                {
+                    ret[cnt] = rcntAbs * rcntAbs;
+                    rcnt--;
+                }
+                else
+                {
+                    ret[cnt] = lcntAbs * lcntAbs;
+                    lcnt++;
+                }
+            }
+            return ret;
+        }
+
+        public int[] DuplicateZeros(int[] arr) {
+            int zeroCnt = 0;
+
+            for (int pointer = 0; pointer < arr.Length-1; pointer++)
+            {
+                if (arr[pointer] == 0)
+                {
+                    zeroCnt++;
+                }
+            }
+            int copyTo = arr.Length - 1, copyFrom = arr.Length -1 - zeroCnt;
+            for (int pointer = arr.Length - 1; pointer > 0; pointer--)
+            {
+
+            }
+
+            return arr;
+        }
+    }
+}
