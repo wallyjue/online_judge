8ac2cd5805b89686a9c7bc44e5a2252abae74361
wallyjue <wallyjue@gmail.com>
2022-03-15 11:45:44 +0800

    Adding 977

---
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem977.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.easy
+{
+    public class Problem977
+    {
+        public int[] SortedSquares(int[] nums)
+        {
+            int[] ret = new int[nums.Length];
+            int lcnt = 0, rcnt = nums.Length - 1;
+            int cnt = nums.Length - 1;
+            for (cnt = nums.Length - 1; cnt >= 0; cnt--)
+            {
+                int lcntAbs = nums[lcnt] >= 0 ? nums[lcnt] : -nums[lcnt];
+                int rcntAbs = nums[rcnt] >= 0 ? nums[rcnt] : -nums[rcnt];
+                if (lcntAbs < rcntAbs)
+                {
+                    ret[cnt] = rcntAbs * rcntAbs;
+                    rcnt--;
+                }
+                else
+                {
+                    ret[cnt] = lcntAbs * lcntAbs;
+                    lcnt++;
+                }
+            }
+            return ret;
+        }
+    }
+}
