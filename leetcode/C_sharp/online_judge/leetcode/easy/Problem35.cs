a932773a660f1757bfda4ef06f980f9917d49bce
wallyjue <wallyjue@gmail.com>
2022-03-15 10:03:43 +0800

    Adding 35

---
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem35.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.easy
+{
+    public class Problem35
+    {
+        public int SearchInsert(int[] nums, int target)
+        {
+            int ret = 0;
+
+
+            return ret;
+        }
+    }
+}
