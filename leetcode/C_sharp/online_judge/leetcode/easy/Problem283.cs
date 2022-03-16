1dd6e0a0871e5989ff36f2f3d49a090b3f8e641f
wallyjue <wallyjue@gmail.com>
2022-03-16 23:42:36 +0800

    Adding new files

---
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem283.cs
@@ -0,0 +1,39 @@
+﻿using online_judge.utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.easy
+{
+    internal class Problem283
+    {
+        public void MoveZeroes(int[] nums)
+        {
+            int ptr = 0, nonzeroPtr = 0;
+            while(nonzeroPtr < nums.Length && ptr < nums.Length)
+            {
+                if (nums[nonzeroPtr] == 0 || ptr > nonzeroPtr)
+                {
+                    nonzeroPtr++;
+                    continue;
+                }
+
+                if (nums[ptr] != 0)
+                {
+                    ptr++;
+                    continue;
+                }
+
+                int temp = nums[ptr];
+                nums[ptr] = nums[nonzeroPtr];
+                nums[nonzeroPtr] = temp;
+                
+                ptr++;
+            }
+
+            Console.WriteLine(Helper.IteratingString(nums));
+        }
+    }
+}
---
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem167.cs
@@ -0,0 +1,39 @@
+﻿using online_judge.utility;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.medium
+{
+    internal class Problem167
+    {
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int index1 = 0, index2 = numbers.Length - 1;
+            while (index1 < index2)
+            {
+                int sum = numbers[index1] + numbers[index2];
+
+                if (sum == target)
+                {
+                    index1++;
+                    index2++;
+                    break;
+                }
+                else if (sum < target)
+                {
+                    ++index1;
+                }
+                else
+                {
+                    --index2;
+                }
+
+            }
+            Console.WriteLine(Helper.IteratingString(new int[] { index1, index2 }));
+            return new int[] { index1, index2 };
+        }
+    }
+}
