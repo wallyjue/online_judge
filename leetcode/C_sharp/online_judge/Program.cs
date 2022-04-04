2f450b89e370881f2363b9020d317e59cc19d01b
wallyjue <wallyjue@gmail.com>
2022-04-04 22:29:10 +0800

    solution

--- leetcode/C_sharp/online_judge/Program.cs
+++ leetcode/C_sharp/online_judge/Program.cs
@@ -18,34 +18,28 @@ namespace online_judge
 				}
 			*/
 			var easy = new online_judge.leetcode.easy.Problem125();
-			var medium = new online_judge.leetcode.medium.Problem24();
+			var medium = new online_judge.leetcode.medium.Problem1721();
 			var explore = new online_judge.leetcode.explore.Array101();
+
+			ListNode node9 = new ListNode(10, null);
+			ListNode node8 = new ListNode(9, node9);
+			ListNode node7 = new ListNode(8, node8);
+			ListNode node6 = new ListNode(7, node7);
+			ListNode node5 = new ListNode(6, node6);
 			
-			//ListNode node5 = new ListNode(6, null);
-			/*
-			ListNode node4 = new ListNode(5, null);
+			ListNode node4 = new ListNode(5, node5);
 			ListNode node3 = new ListNode(4, node4);
 			ListNode node2 = new ListNode(3, node3);
 			ListNode node1 = new ListNode(2, node2);
 			
 			ListNode head = new ListNode(1, node1);
-			*/
-			var ret = easy.IsPalindrome("A man, a plan, a canal: Panama");
+
+			var ret = medium.SwapNodes(head, 6);
 			Console.WriteLine("ret:" + ret);
 
-			ret = easy.IsPalindrome("race a car");
+			ListNode newnode = new ListNode(0, null);
+			ret = medium.SwapNodes(newnode, 1);
 			Console.WriteLine("ret:" + ret);
-
-			ret = easy.IsPalindrome(" ");
-			Console.WriteLine("ret:" + ret);
-
-			ret = easy.IsPalindrome("a.");
-			Console.WriteLine("ret:" + ret);
-
-			ret = easy.IsPalindrome(",.");
-			Console.WriteLine("ret:" + ret);
-
-
 			//ret = explore.ReplaceElements(new int[] { 3, 2,1,9,5,2,7,1,3, 4});
 			//Console.WriteLine("ret:" + ret);
 		}
---
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem1721.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.medium
+{
+    internal class Problem1721
+    {
+        public ListNode SwapNodes(ListNode head, int k)
+        {
+            ListNode headcopy = new ListNode(head.val,head.next);
+            int length = 0;
+            while (headcopy != null)
+            {
+                headcopy = headcopy.next;
+                length++;
+            }
+
+            if (length == 1 || (length % 2 == 1 && k == length / 2 + 1))
+            {
+                return head;
+            }
+
+            if (k > length / 2)
+            {
+                k = length - k + 1;
+            }
+            headcopy = head;
+            ListNode kth = head, swapNode;
+            for (int i = 1; i < length - k + 1; i++)
+            {
+                if (i == k)
+                {
+                    kth = headcopy;
+                }
+                headcopy = headcopy.next;
+            }
+
+            if (k == 1)
+            {
+                head = kth;
+            }
+            swapNode = headcopy;
+            int temp = swapNode.val;
+            swapNode.val = kth.val;
+            kth.val = temp;
+            return head;
+        }
+    }
+}
