22f7ffed98b328d952b6fd5de44a9a049f7bab7f
wallyjue <wallyjue@gmail.com>
2022-04-06 00:15:57 +0800

    solution

---
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem104.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.easy
+{
+    internal class Problem104
+    {
+        public int MaxDepth(TreeNode root)
+        {
+            if (root != null)
+            {
+                Math.Max(MaxDepth(root.left), MaxDepth(root.right));
+            }
+            else
+            {
+                return 1;
+            }
+        }
+    }
+}
