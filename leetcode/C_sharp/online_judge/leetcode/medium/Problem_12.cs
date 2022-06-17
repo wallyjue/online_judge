16b02c9f44b8077b2a6bc85852e3a2971e3db838
wallyjue <wallyjue@gmail.com>
2022-06-17 18:06:09 +0800

    solution

---
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem_12.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.medium
+{
+    internal class Problem_12
+    {
+        public string IntToRoman(int num)
+        {
+            string ret = string.Empty;
+            int dec = num % 10;
+            int ten = (num % 100) / 10;
+            int hundred = (num % 1000) / 100;
+            int thousand = num / 1000;
+
+            return ret;
+        }
+    }
+}
