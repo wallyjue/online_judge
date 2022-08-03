54795f5a4ee2d3789faa428f25b92dca10fb955c
wallyjue <wallyjue@gmail.com>
2022-08-03 13:33:25 +0900

    solution of C Sharp

---
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem145.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.easy
+{
+    internal class Problem145
+    {
+        List<int> route = new List<int>();
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+            PostorderTraverse(root);
+            return route;
+        }
+
+        private void PostorderTraverse(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (node.left != null)
+            {
+                PostorderTraverse(node.left);
+            }
+            if (node.right != null)
+            {
+                PostorderTraverse(node.right);
+            }
+
+            route.Add(node.val);
+        }
+    }
+}
