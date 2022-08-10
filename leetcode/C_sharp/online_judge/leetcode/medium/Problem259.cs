bb667b4bf1116f6a4eb38cc561dcd7af68dc9e58
wallyjue <wallyjue@gmail.com>
2022-08-10 23:29:51 +0900

    solution of C Sharp

--- leetcode/C_sharp/online_judge/leetcode/medium/Problem259.cs
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem259.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,31 @@ namespace online_judge.leetcode.medium
     {
         public int ThreeSumSmaller(int[] nums, int target)
         {
+            int ret = 0;
+            Array.Sort(nums);
+            int right = nums.Length - 1;
+            int left = 0;
+            for (int cnt = 0; cnt < nums.Length;cnt++)
+            {
+                int remaining = target - nums[cnt];
+                right = nums.Length - 1;
+                left = cnt + 1;
+                while (left < right)
+                {
+                    
+                    if (nums[left] + nums[right] < remaining)
+                    {
+                        ret += right - left;
+                        left++;
+                    }
+                    else
+                    {
+                        right--;
+                    }
+                }
+            }
 
+            return ret;
         }
     }
 }
