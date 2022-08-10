427c2473114aed4f2bf0b6f9a7b6a402692a7fe2
wallyjue <wallyjue@gmail.com>
2022-08-11 00:40:23 +0900

    solution of C Sharp

---
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem2370.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.medium
+{
+    internal class Problem2370
+    {
+        public int LongestIdealString(string s, int k)
+        {
+
+        }
+    }
+}
