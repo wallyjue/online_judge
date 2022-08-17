d7801c0da9197e370c7d7ed19efc74c61813ab31
wallyjue <wallyjue@gmail.com>
2022-08-17 17:25:37 +0900

    solution of C Sharp

---
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem744.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.easy
+{
+    internal class Problem744
+    {
+        public char NextGreatestLetter(char[] letters, char target)
+        {
+            char ret = 'a';
+
+            return ret;
+        }
+    }
+}
