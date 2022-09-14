0aa87683e5ce9e13fa25f0d26163b927963e9ff6
wallyjue <wallyjue@gmail.com>
2022-09-14 16:50:33 +0900

    solution of C Sharp

---
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem253.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.medium
+{
+    internal class Problem253
+    {
+        public int MinMeetingRooms(int[][] intervals)
+        {
+            int ret = 0;
+            Array.Sort(intervals, this.Compare);
+
+            return ret;
+        }
+
+        private int Compare(int[] a, int[] b)
+        {
+            if (a[0] > b[0])
+            {
+                return 1;
+            }
+            else if (b[0] > a[0])
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+}
