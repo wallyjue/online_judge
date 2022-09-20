5fff6359c138db05a4d31f05e56fd509a9ce0d99
wallyjue <wallyjue@gmail.com>
2022-09-20 22:09:20 +0900

    solution of C Sharp

---
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem451.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.medium
+{
+    internal class Problem451
+    {
+        public string FrequencySort(string s)
+        {
+            StringBuilder sb = new StringBuilder(string.Empty, s.Length);
+            PriorityQueue<char, int> pq = new PriorityQueue<char, int>(Comparer<int>.Create((x, y) => y - x));
+            int[] table = new int[128];
+            for (int cnt = 0; cnt < s.Length; cnt++)
+            {
+                table[s[cnt]]++;
+            }
+
+            for(int index = 0;index < 128;index++)
+            {
+                if (table[index] > 0)
+                {
+                    pq.Enqueue((char)index, table[index]);
+                }
+            }
+
+            while(pq.Count > 0)
+            {
+                char element;
+                int times;
+                pq.TryDequeue(out element, out times);
+                sb.Append(element, times);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem973.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.medium
+{
+    internal class Problem973
+    {
+        public int[][] KClosest(int[][] points, int k)
+        {
+            int[][] ret = new int[k][];
+            return ret;
+        }
+    }
+}
