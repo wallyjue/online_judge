2ef2971551a0631b74ebf162fbc46de3b9e121ff
wallyjue <wallyjue@gmail.com>
2022-09-26 13:13:53 +0900

    solution of C Sharp

---
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem20.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.easy
+{
+    internal class Problem20
+    {
+        public bool IsValid(string s)
+        {
+            bool ret = false;
+
+            return ret;
+        }
+    }
+}
