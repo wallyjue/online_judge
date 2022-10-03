268e4ca9f62d6d9b4c6d9bdae2a2a615ffa79fe7
wallyjue <wallyjue@gmail.com>
2022-10-03 14:31:24 +0900

    solution of C Sharp

---
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem103.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.medium
+{
+    internal class Problem103
+    {
+        public IList<IList<int>> ZigzagLevelOrder(TreeNode root)
+        {
+            List<IList<int>> ret = new List<IList<int>>();
+            if (root == null)
+            {
+                return ret;
+            }
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+            bool leftToRight = true;
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                LinkedList<int> layer = new LinkedList<int>();
+                int count = queue.Count;
+                for (int cnt = 0; cnt < count; cnt++)
+                {
+                    TreeNode node = queue.Dequeue();
+                    if (leftToRight)
+                    {
+                        layer.AddLast(node.val);
+                    }
+                    else
+                    {
+                        layer.AddFirst(node.val);
+                    }
+
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+                leftToRight = !leftToRight;
+                ret.Add(layer.ToList());
+            }
+
+            return ret;
+        }
+    }
+}
---
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem107.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.medium
+{
+    internal class Problem107
+    {
+        public IList<IList<int>> LevelOrderBottom(TreeNode root)
+        {
+            List<IList<int>> ret = new List<IList<int>>();
+            Stack<IList<int>> stack = new Stack<IList<int>>();
+            if (root == null)
+            {
+                return ret;
+            }
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+
+            queue.Enqueue(root);
+
+            while (queue.Count > 0)
+            {
+                List<int> layer = new List<int>();
+                int count = queue.Count;
+                for (int cnt = 0; cnt < count; cnt++)
+                {
+                    TreeNode node = queue.Dequeue();
+                    layer.Add(node.val);
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+                stack.Push(layer);
+            }
+
+            while(stack.Count > 0)
+            {
+                IList<int> layer = stack.Pop();
+
+                ret.Add(layer);
+            }
+
+            return ret;
+        }
+    }
+}
