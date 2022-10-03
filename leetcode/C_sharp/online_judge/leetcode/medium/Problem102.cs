e986502c13f1aea0a4341fac91f9354b0a82bb98
wallyjue <wallyjue@gmail.com>
2022-10-03 12:55:56 +0900

    solution of C Sharp

---
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem102.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.medium
+{
+    internal class Problem102
+    {
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+            List<IList<int>> ret = new List<IList<int>>();
+            if (root == null)
+            {
+                return ret;
+            }
+            Queue<TreeNode> queue = new Queue<TreeNode>();
+
+            queue.Enqueue(root);
+
+            while(queue.Count > 0)
+            {
+                List<int> layer = new List<int>();
+                int count = queue.Count;
+                for (int cnt = 0; cnt < count; cnt++)
+                {
+                    TreeNode node = queue.Dequeue();
+                    layer.Add(node.val);
+                    if (node.left != null)
+                    {
+                        queue.Enqueue(node.left);
+                    }
+
+                    if (node.right != null)
+                    {
+                        queue.Enqueue(node.right);
+                    }
+                }
+
+                ret.Add(layer);
+            }
+
+            return ret;
+        }
+    }
+}
