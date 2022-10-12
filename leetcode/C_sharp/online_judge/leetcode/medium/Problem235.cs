da5998dbd0df95c3dfba5c63e71c8795a13c7810
wally.chang <wally.chang@protolabs.co.jp>
2022-10-12 17:38:39 +0900

    Update

--- leetcode/C_sharp/online_judge/leetcode/medium/Problem235.cs
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem235.cs
@@ -10,9 +10,43 @@ namespace online_judge.leetcode.medium
     {
         public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q)
         {
-            TreeNode ret = new TreeNode();
+            TreeNode ret = null;
+            var list1 = traverse(root, p);
+            var list2 = traverse(root, q);
 
+            list1.Reverse();
+            foreach(var item in list1)
+            {
+                ret = list2.FirstOrDefault(x => x.val == item.val);
+                if (ret != null) return ret;
+            }
             return ret;
+        }
+
+        private List<TreeNode> traverse(TreeNode start, TreeNode target)
+        {
+            List<TreeNode> list = new List<TreeNode>();
+            while (start != null)
+            {
+                list.Add(start);
+
+                if (start.val == target.val)
+                {
+                    return list;
+                }
+
+                if (start.left != null && target.val < start.val)
+                {
+                    start = start.left;
+                }
+
+                if (start.right != null && target.val > start.val)
+                {
+                    start = start.right;
+                }
+            }
+
+            return list;
         }
     }
 }
