e8bb17aef13688d87902f0c42a89feafc50452da
Wally Chang <wallyjue@gmail.com>
2022-10-18 23:16:05 +0900

    solution of C Sharp

--- leetcode/C_sharp/online_judge/leetcode/easy/Problem844.cs
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem844.cs
@@ -10,7 +10,33 @@ namespace online_judge.leetcode.easy
     {
         public bool BackspaceCompare(string s, string t)
         {
-            return false;
+            StringBuilder sb1 = new StringBuilder();
+            for (int cnt = 0; cnt < s.Length ;cnt++)
+            {
+                if (s[cnt] != '#')
+                {
+                    sb1.Append(s[cnt]);
+                }
+                else if (sb1.Length > 0)
+                {
+                    sb1.Remove(sb1.Length - 1, 1);
+                }
+            }
+
+            StringBuilder sb2 = new StringBuilder();
+            for (int cnt = 0; cnt < t.Length; cnt++)
+            {
+                if (t[cnt] != '#')
+                {
+                    sb2.Append(t[cnt]);
+                }
+                else if (sb2.Length > 0)
+                {
+                    sb2.Remove(sb2.Length - 1, 1);
+                }
+            }
+
+            return sb1.ToString() == sb2.ToString();
         }
     }
 }
