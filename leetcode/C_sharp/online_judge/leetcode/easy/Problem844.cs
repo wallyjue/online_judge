981f7d0877e4f487d276ae91a4afee185df329cd
wallyjue <wallyjue@gmail.com>
2022-10-18 21:30:15 +0900

    solution of C Sharp

---
+++ leetcode/C_sharp/online_judge/leetcode/easy/Problem844.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.easy
+{
+    internal class Problem844
+    {
+        public bool BackspaceCompare(string s, string t)
+        {
+            return false;
+        }
+    }
+}
---
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem394.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.medium
+{
+    internal class Problem394
+    {
+        public string DecodeString(string s)
+        {
+            string ret = string.Empty;
+
+            return ret;
+        }
+    }
+}
