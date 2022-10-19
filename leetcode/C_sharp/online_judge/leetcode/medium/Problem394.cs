d52f5cbf86641a8892127291ce59a255054bce0a
wallyjue <wallyjue@gmail.com>
2022-10-19 12:04:39 +0900

    solution of C Sharp

--- leetcode/C_sharp/online_judge/leetcode/medium/Problem394.cs
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem394.cs
@@ -10,7 +10,32 @@ namespace online_judge.leetcode.medium
     {
         public string DecodeString(string s)
         {
+            Stack<char> stack = new Stack<char>();
+
             string ret = string.Empty;
+            for (int cnt = 0; cnt < s.Length; cnt++)
+            {
+                if (s[cnt] != ']')
+                {
+                    stack.Push(s[cnt]);
+                }
+                else
+                {
+                    StringBuilder sb = new StringBuilder();
+                    int rep = 0;
+                    while (stack.Count > 0 && stack.Peek() > '9' && stack.Peek() < '0')
+                    {
+                        char c = stack.Pop();
+                        if (c != '[')
+                        {
+                            sb.Append(c);
+                        }
+                    }
+
+                    rep = int.Parse(stack.Pop().ToString());
+                }
+            }
+
 
             return ret;
         }
