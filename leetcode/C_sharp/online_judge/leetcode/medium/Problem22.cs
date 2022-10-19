83912ea6d93276216aaa37aba8a693ac85e6d68b
wallyjue <wallyjue@gmail.com>
2022-10-20 00:08:32 +0900

    solution of C Sharp

--- leetcode/C_sharp/online_judge/leetcode/medium/Problem22.cs
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem22.cs
@@ -9,45 +9,38 @@ namespace online_judge.leetcode.medium
 {
     internal class Problem22
     {
-        // dynamic programming function
-        // f(n) = (f(n-1)) + ()f(n-1) + f(n-1)();
-        Hashtable table;
         public IList<string> GenerateParenthesis(int n)
         {
-            table = new Hashtable();
-            table.Add(1, new List<string> { "()" });
-            if (n == 1)
-            {
-                return (IList<string>)table[1];
-            }
-
-            for(int cnt = 2; cnt <= n; cnt++)
-            {
-                dynamicProgramming(cnt);
-            }
-
-            return (IList<string>)table[n];
+            List<string> result = new List<string>();
+            backtracking(result, new StringBuilder(), 0, 0, n);
+            return result;
         }
 
-        private void dynamicProgramming(int level)
+        private void backtracking(List<string> set, StringBuilder sb, int open, int close, int level)
         {
-            var prev = (List<string>)table[level - 1];
-            var ret = new List<string>();
-            foreach (string item in prev)
+            // ending condition
+            if (sb.Length == level * 2)
             {
-                ret.Add($"({item})");
-                ret.Add($"(){item}");
-                ret.Add($"{item}()");
-                ret = ret.Distinct().ToList();
-            }
-            string rep = string.Empty;
-            for (int cnt = 0; cnt < level; cnt++)
-            {
-                rep += "()";
+                set.Add(sb.ToString());
+                return;
             }
 
-            ret.Remove(rep);
-            table.Add(level, ret);
+
+            if (level > open)
+            {
+                sb.Append("(");
+                backtracking(set, sb, open + 1, close, level);
+                sb.Remove(sb.Length - 1, 1);
+            }
+
+            if (open > close)
+            {
+                sb.Append(")");
+                backtracking(set, sb, open, close + 1, level);
+                sb.Remove(sb.Length - 1, 1);
+            }
+
+            
         }
     }
 }
