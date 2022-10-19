bf8f806f00127083f0605fe3ac0796238d07b0a4
wallyjue <wallyjue@gmail.com>
2022-10-19 16:07:48 +0900

    solution of C Sharp

---
+++ leetcode/C_sharp/online_judge/leetcode/hard/Problem1770.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.hard
+{
+    internal class Problem1770
+    {
+        public int MaximumScore(int[] nums, int[] multipliers)
+        {
+            return 0;
+        }
+    }
+}
