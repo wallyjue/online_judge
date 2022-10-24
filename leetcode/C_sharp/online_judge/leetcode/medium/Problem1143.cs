59e65501b4d25705cd9cd47b90e98df6e440ba66
wallyjue <wallyjue@gmail.com>
2022-10-24 17:00:12 +0900

    solution of C Sharp

---
+++ leetcode/C_sharp/online_judge/leetcode/medium/Problem1143.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace online_judge.leetcode.medium
+{
+    internal class Problem1143
+    {
+        public int LongestCommonSubsequence(string text1, string text2)
+        {
+            return 0;
+        }
+    }
+}
