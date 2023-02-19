91636e58e7715717779cc83ec78158f8188f878a
Wally Chang <wallyjue@gmail.com>
2023-02-19 10:50:54 +0900

    solution of Java

--- leetcode/Java/projectMaven/src/main/java/com/leetcode/wallyjue/easy/Problem13.java
+++ leetcode/Java/projectMaven/src/main/java/com/leetcode/wallyjue/easy/Problem13.java
@@ -1,2 +1,87 @@
-package com.leetcode.wallyjue.easy;public class Problem13 {
+package com.leetcode.wallyjue.easy;
+
+public class Problem13 {
+    public int romanToInt(String s) {
+        int ret = 0;
+        char prev = 'a';
+        for(int index = 0; index < s.length();index++)
+        {
+            switch(s.charAt(index))
+            {
+                default:
+                    break;
+                case 'M':
+                    if (prev == 'C')
+                    {
+                        ret += 800;
+                    }
+                    else
+                    {
+                        ret += 1000;
+                    }
+                    prev = 'M';
+                    break;
+                case 'D':
+                    if (prev == 'C')
+                    {
+                        ret += 300;
+                    }
+                    else
+                    {
+                        ret += 500;
+                    }
+                    prev = 'D';
+                    break;
+                case 'C':
+                    if (prev == 'X')
+                    {
+                        ret += 80;
+                    }
+                    else
+                    {
+                        ret += 100;
+                    }
+                    prev = 'C';
+                    break;
+                case 'X':
+                    if (prev == 'I')
+                    {
+                        ret += 8;
+                    }
+                    else
+                    {
+                        ret += 10;
+                    }
+                    prev = 'X';
+                    break;
+                case 'I':
+                    prev = 'I';
+                    ret += 1;
+                    break;
+                case 'L':
+                    if (prev == 'X')
+                    {
+                        ret += 30;
+                    }
+                    else
+                    {
+                        ret += 50;
+                    }
+                    prev = 'L';
+                    break;
+                case 'V':
+                    if (prev == 'I')
+                    {
+                        ret += 3;
+                    }
+                    else
+                    {
+                        ret += 5;
+                    }
+                    prev = 'V';
+                    break;
+            }
+        }
+        return ret;
+    }
 }
